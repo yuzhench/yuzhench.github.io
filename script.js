4e8eee0e491688792be56a3777f65f1bf0e097fc
yuzhen chen <yuzhench@umich.edu>
2024-05-27 00:22:35 -0400

    may27 update

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+myButton.addEventListener(
+  "click",
+  function () {
+      myPopup.classList.add("show");
+  }
+);
+closePopup.addEventListener(
+  "click",
+  function () {
+      myPopup.classList.remove(
+          "show"
+      );
+  }
+);
+window.addEventListener(
+  "click",
+  function (event) {
+      if (event.target == myPopup) {
+          myPopup.classList.remove(
+              "show"
+          );
+      }
+  }
+);
